78e57bd5f0829e424a5aab145f4315a3f033528a
SekianZ <sebasb256@gmail.com>
2025-10-21 19:37:50 -0500

    feat: :sparkles: Implementacion de rutas de categorias

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,25 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 /*  PRODUCTOS   */
+
 Route::prefix('products')->middleware(['auth:sanctum'])->group(function () {
     // CRUD básico
     Route::post('/', [ProductController::class, 'store']);
 
+});
+
+/* CATEGORIAS */
+Route::prefix('categories')->middleware(['auth:sanctum'])->group(function() {
+    Route::get('/', [CategoryController::class, 'index']);
+    Route::get('/tree', [CategoryController::class, 'tree']);
+    Route::get('/{id}', [CategoryController::class, 'show']);
+    Route::post('/', [CategoryController::class, 'store']);
+    Route::match(['put', 'patch'], '/{id}', [CategoryController::class, 'update']);
+    Route::delete('/{id}', [CategoryController::class, 'destroy']);
 });
