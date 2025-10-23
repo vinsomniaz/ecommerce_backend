b78d12a9784b0630560726b45fae820b765ee77c
Sekian <datastore.sac.shop@gmail.com>
2025-10-23 17:16:51 -0500

    feat: :sparkles: Nuevas rutas endpoints para CRUD de almacenes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\WarehouseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,4 +23,14 @@ Route::prefix('categories')->middleware(['auth:sanctum'])->group(function() {
     Route::post('/', [CategoryController::class, 'store']);
     Route::match(['put', 'patch'], '/{id}', [CategoryController::class, 'update']);
     Route::delete('/{id}', [CategoryController::class, 'destroy']);
+});
+
+/* ALMACENES */
+Route::prefix('warehouses')->middleware(['auth:sanctum'])->group(function() {
+    Route::get('/', [WarehouseController::class, 'index']);
+    Route::post('/', [WarehouseController::class, 'store']);
+    Route::get('/{id}', [WarehouseController::class, 'show']);
+    Route::put('/{id}', [WarehouseController::class, 'update']);
+    Route::patch('/{id}', [WarehouseController::class, 'update']);
+    Route::delete('/{id}', [WarehouseController::class, 'destroy']);
 });
